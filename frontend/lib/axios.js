211299cdcc6ff6a964f445a807439d8921cce0e2
Daniel <cronobr5@gmail.com>
2025-02-13 18:57:36 -0300

    lib corretion

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "production" ? "https://corp2.onrender.com/api" : "/api",
-	withCredentials: true,
+const API_URL = "https://corp2.onrender.com/api"; 
+
+const instance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true, 
 });
 
-export default axiosInstance;
+export default instance;
